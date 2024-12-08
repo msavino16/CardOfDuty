3e30ef071d2d4a96e129adeea214f7bbcb745d27
msavino16 <130592777+msavino16@users.noreply.github.com>
2024-12-08 16:29:29 -0500

    Added some function docstrings

--- Card.py
+++ Card.py
@@ -1,11 +1,24 @@
 class Card:
+    
     def __init__(self, name, attack=0, defense=0, healing=0):
+        
         self.name = name
         self.attack = attack
         self.defense = defense
         self.healing = healing
 
     def apply_effect(self, target_player, current_player):
+        """
+        Function to deal damage, increase defense, or Heal
+        
+        :param target_player: The player to be affected
+        :type target_player: Player
+        
+        :param target_player: The player using the card
+        :type target_player: Player
+        
+        """
+
         if self.attack > 0:
             target_player.take_damage(self.attack)
 
